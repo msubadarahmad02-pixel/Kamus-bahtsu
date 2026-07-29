534efb976f60cd64027d606ad54e0bf1239a8cbf
msubadarahmad02-pixel <msubadar.ahmad02@gmail.com>
2026-07-30 06:49:58 +0700

    Update baca_quran.js

--- baca_quran.js
+++ baca_quran.js
@@ -1,7 +1,7 @@
 // ==========================================
-// KONFIGURASI SERVING GAMBAR (OTOMATIS DIKOMPRES LEWAT WSRV.NL)
+// KONFIGURASI SERVING GAMBAR (DIKOMPRES & DIIZINKAN REDIRECT)
 // ==========================================
-const BASE_URL_GAMBAR = "https://wsrv.nl/?w=1080&output=webp&url=https://github.com/msubadarahmad02-pixel/Kamus-bahtsu/releases/download/v1.0/";
+const BASE_URL_GAMBAR = "https://wsrv.nl/?w=1080&output=webp&n=-1&url=https://github.com/msubadarahmad02-pixel/Kamus-bahtsu/releases/download/v1.0/";
 
 document.addEventListener('DOMContentLoaded', () => {
     const urlParams = new URLSearchParams(window.location.search);
